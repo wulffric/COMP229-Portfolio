8e2c9593ef30b47dfe4ce4fa4651e7a51df2c55d
Abass Ajibola <aajibola@my.centennialcollege.ca>
2025-10-31 16:09:31 -0400

    Remove auth middleware from project routes for testing

--- server/routes/projectRoutes.js
+++ server/routes/projectRoutes.js
@@ -1,14 +1,13 @@
 import { Router } from "express";
 import * as p from "../controllers/projectController.js";
-import { requireAuth } from "../middleware/auth.js";
 
 const router = Router();
 
 router.get("/", p.getAll);
 router.get("/:id", p.getById);
-router.post("/", requireAuth, p.create);
-router.put("/:id", requireAuth, p.update);
-router.delete("/:id", requireAuth, p.remove);
-router.delete("/", requireAuth, p.removeAll);
+router.post("/", p.create);
+router.put("/:id", p.update);
+router.delete("/:id", p.remove);
+router.delete("/", p.removeAll);
 
 export default router;
