75dfd4bc84d906bdd67159d2e1b94a407d0a3035
Abass Ajibola <aajibola@my.centennialcollege.ca>
2025-10-31 15:29:30 -0400

    Remove comments and make contact routes public for testing

--- server/routes/contactRoutes.js
+++ server/routes/contactRoutes.js
@@ -1,17 +1,13 @@
 import { Router } from "express";
 import * as c from "../controllers/contactController.js";
-import { requireAuth } from "../middleware/auth.js";
 
 const router = Router();
 
-// public reads
 router.get("/", c.getAll);
 router.get("/:id", c.getById);
-
-// protected writes
-router.post("/", requireAuth, c.create);
-router.put("/:id", requireAuth, c.update);
-router.delete("/:id", requireAuth, c.remove);
-router.delete("/", requireAuth, c.removeAll);
+router.post("/", c.create);
+router.put("/:id", c.update);
+router.delete("/:id", c.remove);
+router.delete("/", c.removeAll);
 
 export default router;
